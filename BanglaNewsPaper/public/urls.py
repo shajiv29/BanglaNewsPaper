bcfb92f60c3329804e6951e782e7491fd5b0935b
Md. Sazibur Rahman <36544022+shajiv29@users.noreply.github.com>
2022-01-22 13:23:21 +0600

    change url only when add class based   # path('detail/<int:pk>/', news_details),     path('detail/<int:id>/', NewsDetails.as_view()),

--- BanglaNewsPaper/public/urls.py
+++ BanglaNewsPaper/public/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import news_list, news_details, NewsAPIView
+from .views import news_list, news_details, NewsAPIView, NewsDetails
 
 urlpatterns = [
     # Method based call
     # path('news/', news_list),
     path('news/', NewsAPIView.as_view()),
-    path('detail/<int:pk>/', news_details),
+    # path('detail/<int:pk>/', news_details),
+    path('detail/<int:id>/', NewsDetails.as_view()),
 ]
